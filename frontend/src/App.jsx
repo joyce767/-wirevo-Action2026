802d0cfbf7d2bfeec323ec59c406be23f186d366
Joy Hephzibah <hephzibahjoy01@gmail.com>
2025-11-26 12:06:15 +0100

    Add logo to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import { saveAs } from "file-saver";
 import "./App.css";
+import logo from "/assets/evo.png";
 
 const backendUrl = "http://localhost:5000"; // Change to live backend URL when deployed
 
